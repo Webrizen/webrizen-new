9509deb756735d42ff40c59c9399b746504b4fb9
WebRizen <webrizen@gmail.com>
2023-08-13 21:04:56 +0530

    New Sections & Images added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import "./styles/App.css";
 import HeroIMG from "./assets/hero.webp";
-import { BsFillBuildingsFill } from "react-icons/bs";
+import { BsFillBuildingsFill, BsCheckCircle, BsQuestion } from "react-icons/bs";
 import { AiOutlineDoubleRight } from "react-icons/ai";
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import BeOne from "./assets/beone.webp";
+import BrightTranslations from "./assets/brighttranslations.webp";
+import codecashflow from "./assets/codecashflow.webp";
+import healthcarebiodiversity from "./assets/healthcarebiodiversity.webp";
+import bimaccanada from "./assets/bimaccanada.webp";
+import mycasaforte from "./assets/mycasaforte.webp";
+import emma from "./assets/emma.webp";
+import HireUs from './assets/hire-us.webp'
 
 function App() {
   const [ref, inView] = useInView({
@@ -64,9 +72,7 @@ function App() {
         <span></span>
         <span></span>
       </div>
-      <motion.section
-        className="hero"
-      >
+      <motion.section className="hero">
         <motion.div
           className="leftHero"
           initial={{ opacity: 0, x: -50 }}
@@ -92,7 +98,11 @@ function App() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5, delay: 0.5 }}
           >
-            Embark on a digital journey where innovation meets functionality. At WebRizen, we're more than developers; we're architects of exceptional online experiences. With unwavering passion and cutting-edge expertise, we transform your visionary concepts into tangible web solutions.
+            Embark on a digital journey where innovation meets functionality. At
+            WebRizen, we're more than developers; we're architects of
+            exceptional online experiences. With unwavering passion and
+            cutting-edge expertise, we transform your visionary concepts into
+            tangible web solutions.
           </motion.p>
           <motion.button
             initial={{ opacity: 0, y: -20 }}
@@ -141,7 +151,13 @@ function App() {
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.5, delay: 0.6 }}
         >
-          At WebRizen, we understand that every business is unique, which is why we offer custom web design and development packages that are tailored to your specific needs. Our team of experienced designers and developers work closely with you to create a website that is not only visually stunning but also functional and user-friendly. From e-commerce sites to custom CMS builds, our packages are designed to meet your specific requirements and budget.
+          At WebRizen, we understand that every business is unique, which is why
+          we offer custom web design and development packages that are tailored
+          to your specific needs. Our team of experienced designers and
+          developers work closely with you to create a website that is not only
+          visually stunning but also functional and user-friendly. From
+          e-commerce sites to custom CMS builds, our packages are designed to
+          meet your specific requirements and budget.
         </motion.p>
         <motion.div
           className="threecol"
@@ -149,23 +165,44 @@ function App() {
           animate={inView ? { opacity: 1, y: 0 } : {}}
           transition={{ duration: 0.5, delay: 0.8 }}
         >
-          <motion.div
-            className="card"
-          >
+          <motion.div className="card">
             <h3>Web Applications Design & Development</h3>
-            <p>Elevate your online presence with our expert Website Design & Development services. From visionary concepts to captivating user experiences, we craft websites that resonate. Discover innovation, creativity, and seamless functionality - all in one place. Let's shape your digital future together. Harness the power of Next.js, React.js, PHP, Python, Firebase, MongoDB, Node.js, and more to build dynamic, innovative online solutions. Transforming your vision into reality, one pixel at a time.</p>
+            <p>
+              Elevate your online presence with our expert Website Design &
+              Development services. From visionary concepts to captivating user
+              experiences, we craft websites that resonate. Discover innovation,
+              creativity, and seamless functionality - all in one place. Let's
+              shape your digital future together. Harness the power of Next.js,
+              React.js, PHP, Python, Firebase, MongoDB, Node.js, and more to
+              build dynamic, innovative online solutions. Transforming your
+              vision into reality, one pixel at a time.
+            </p>
           </motion.div>
-          <motion.div
-            className="card"
-          >
+          <motion.div className="card">
             <h3>Mobile Applications Design & Development</h3>
-            <p>palm of your customers' hands with WebRizen's Mobile Application Design and Development services. Harness cutting-edge technology and creativity to transform your ideas into sleek, intuitive, and impactful mobile applications. Our expert team is dedicated to engineering seamless user journeys, stunning interfaces, and robust functionalities that captivate and engage your audience. Unlock the potential of mobile innovation with WebRizen. Let's shape the future of mobile together.</p>
+            <p>
+              palm of your customers' hands with WebRizen's Mobile Application
+              Design and Development services. Harness cutting-edge technology
+              and creativity to transform your ideas into sleek, intuitive, and
+              impactful mobile applications. Our expert team is dedicated to
+              engineering seamless user journeys, stunning interfaces, and
+              robust functionalities that captivate and engage your audience.
+              Unlock the potential of mobile innovation with WebRizen. Let's
+              shape the future of mobile together.
+            </p>
           </motion.div>
-          <motion.div
-            className="card"
-          >
+          <motion.div className="card">
             <h3>Computer Applications Design & Development</h3>
-            <p>At WebRizen, we specialize in turning your ideas into cutting-edge computer applications. Our dedicated team of experts is driven by innovation and creativity, working tirelessly to design and develop solutions that transcend expectations. Whether you need a sleek and intuitive user interface or a robust backend architecture, we've got you covered. Elevate your digital presence with WebRizen and experience the true artistry of computer application design and development.</p>
+            <p>
+              At WebRizen, we specialize in turning your ideas into cutting-edge
+              computer applications. Our dedicated team of experts is driven by
+              innovation and creativity, working tirelessly to design and
+              develop solutions that transcend expectations. Whether you need a
+              sleek and intuitive user interface or a robust backend
+              architecture, we've got you covered. Elevate your digital presence
+              with WebRizen and experience the true artistry of computer
+              application design and development.
+            </p>
           </motion.div>
         </motion.div>
         <motion.button
@@ -175,6 +212,90 @@ function App() {
         >
           More Details
         </motion.button>
+        <br />
+        <motion.h2
+          className="servicesh2"
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5, delay: 0.4 }}
+        >
+          Trusted by leading companies
+        </motion.h2>
+        <motion.div
+          initial={{ opacity: 0, y: -20 }}
+          animate={inView ? { opacity: 1, y: 0 } : {}}
+          transition={{ duration: 0.5, delay: 0.4 }}
+          className="clients"
+        >
+          <img src={BeOne} alt="BeOne" />
+          <img src={BrightTranslations} alt="brighttranslations" />
+          <img src={codecashflow} alt="codecashflow" />
+          <img src={healthcarebiodiversity} alt="healthcarebiodiversity" />
+          <img src={bimaccanada} alt="bimaccanada" />
+          <img src={mycasaforte} alt="mycasaforte" />
+          <img src={emma} alt="emma" />
+        </motion.div>
+        <motion.div className="twocol">
+          <motion.div
+            className="leftHero"
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          >
+            <motion.span
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.3 }}
+            >
+              <BsQuestion /> Why Us?
+            </motion.span>
+            <motion.h1
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              Big Brands Turn To Us To Elevate Their Digital Presence.
+            </motion.h1>
+            <motion.p
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.5 }}
+            >
+              From publicly traded enterprises to mom and pop e-commerce
+              boutiques, our results speak for themselves. From Website
+              Development and Mobile Application Development to full-funnel
+              optimization and testing, we’re focused on your profitability and
+              achieving your desired goals.
+              <ul>
+                <li><BsCheckCircle/> Creative Problem Solvers</li>
+                <li><BsCheckCircle/> Client-Centric Approach</li>
+                <li><BsCheckCircle/> Proven Track Record</li>
+              </ul>
+            </motion.p>
+            <motion.button
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.5, delay: 0.6 }}
+            >
+              <AiOutlineDoubleRight />
+              Testimonails
+            </motion.button>
+          </motion.div>
+          <motion.div
+            className="rightHero"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.6, delay: 0.2 }}
+          >
+            <motion.img
+              src={HireUs}
+              alt="Hero Image"
+              initial={{ scale: 0.9, opacity: 0 }}
+              animate={{ scale: 1, opacity: 1 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            />
+          </motion.div>
+        </motion.div>
       </section>
     </>
   );
