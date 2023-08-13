1e30cab9738fa5c097ccbb0f3368489bd5eeb7e0
WebRizen <webrizen@gmail.com>
2023-08-13 17:21:06 +0530

    updated hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,87 @@
-import './styles/App.css';
+import "./styles/App.css";
+import HeroIMG from "./assets/hero.png";
+import { BsFillBuildingsFill } from "react-icons/bs";
+import { AiOutlineDoubleRight } from "react-icons/ai";
 
 function App() {
-
   return (
     <>
-      <section>
-        hello World!
+      <div className="background">
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <section className="hero">
+        <div className="leftHero">
+          <span>
+            <BsFillBuildingsFill /> Elevating Your Digital Presence
+          </span>
+          <h1>Crafting Dynamic Web Applications That Inspire.</h1>
+          <p>
+            Embark on a digital journey where innovation meets functionality. At
+            WebRizen, we're more than developers; we're architects of
+            exceptional online experiences. With unwavering passion and
+            cutting-edge expertise, we transform your visionary concepts into
+            tangible web solutions.
+          </p>
+          <button>
+            <AiOutlineDoubleRight />
+            Discover the Possibilities
+          </button>
+        </div>
+        <div className="rightHero">
+          <img src={HeroIMG} alt="Hero Image" />
+        </div>
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
