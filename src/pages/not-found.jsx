30f1adf3b8c6247383cdbaac88f1d4642490489b
WebRizen <webrizen@gmail.com>
2023-12-28 09:26:55 +0530

    Updated website for responsive design and Tailwind CSS integration
    - Integrated Tailwind CSS to streamline styling and improve the
    responsive design.
    - Refined 404 Not Found page with Tailwind-styled components for
    enhanced user experience.
    - Adopted utility-first CSS framework for maintainable and scalable
    styling.
    - Configured project dependencies for PostCSS and Autoprefixer to ensure
    cross-browser compatibility.
    - Enriched HTML structure with meta tags to optimize connections to
    Google Fonts.
    - Reformatted codebase to align with Tailwind CSS conventions and
    removed unused font imports.

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -3,10 +3,22 @@ import Layout from '../component/layout';
 
 export default function NotFound() {
   return (
-    <>
     <Layout>
-    404 Not Found
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-6xl font-semibold mb-4">404</h1>
+          <p className="text-lg mb-8">Page not found</p>
+          <p className="text-gray-500 mb-8">
+            The page you are looking for might be under construction or does not exist.
+          </p>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-blue"
+            onClick={() => window.history.back()}
+          >
+            Go Back
+          </button>
+        </div>
+      </div>
     </Layout>
-    </>
-  )
+  );
 }
